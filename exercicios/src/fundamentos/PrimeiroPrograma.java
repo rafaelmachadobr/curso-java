e8a4e2109cdfcfef488a6a4a294889d488d6ca50
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-10 19:28:01 -0300

    Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,30 @@
 package fundamentos;
 
+/*
+ * Essa classe representa... JAVADOC
+ * 
+ * @author Rafael
+ * @since JDK1.0
+ */
+
 public class PrimeiroPrograma {
+	
+	/*
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
 		System.out.println("Ola, Mundo!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3...
+		 */
+		
 		System.out.println("Segunda linha...");
+		System.out.println("Fim");
 	}
 }
