1ef14b8cbfdce1d3ad266424a7c0280fa3617cf5
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-11 17:20:29 -0300

    Inferência de Tipos

---
+++ exercicios/src/fundamentos/inferencia.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class inferencia {
+
+	public static void main(String[] args) {
+		
+		double a = 5.7;
+		System.out.println(a);
+		
+		var b = 8.2;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		
+		c = "Outro texto";
+		System.out.println(c);
+	
+		int d;
+		d = 19;
+		System.out.println(d);
+	}
+}
