95212275643a276c383283046239651ea9ff996d
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-06-10 19:35:15 -0300

    Operadores Relacionais

--- exercicios/src/fundamentos/operadores/DesafioLogicos.java
+++ exercicios/src/fundamentos/operadores/DesafioLogicos.java
@@ -7,16 +7,16 @@ public class DesafioLogicos {
 		boolean trabalho1 = true;
 		boolean trabalho2 = false;
 		
-		System.out.println("Comprar TV de 50 polegadas:");
+		System.out.println("Comprou uma TV de 50 polegadas:");
 		System.out.println(trabalho1 && trabalho2);
 		
-		System.out.println("Comprar TV de 32 polegadas:");
+		System.out.println("Comprou uma TV de 32 polegadas:");
 		System.out.println(trabalho1 ^ trabalho2);
 		
-		System.out.println("Tomar sorvete com a familia:");
+		System.out.println("Tomou sorvete com a familia no shopping:");
 		System.out.println(trabalho1 || trabalho2);
 		
-		System.out.println("Familia ficar em casa:");
+		System.out.println("A familia ficou em casa:");
 		System.out.println(!trabalho1 && !trabalho2);
 	}
 }
