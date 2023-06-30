c6cd7028d5ae009d0e7537449916a35dd42afc04
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-06-30 20:37:18 -0300

    IF

---
+++ exercicios/src/controle/If.java
@@ -0,0 +1,21 @@
+package controle;
+
+import java.util.Scanner;
+
+public class If {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite a média: ");
+		double media = entrada.nextDouble();
+		
+		if (media <= 10 && media >= 7) {
+			System.out.println("Aprovado!");
+			System.out.println("Parabéns!");
+		}
+		
+		entrada.close();
+	}
+}
