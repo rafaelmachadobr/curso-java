cda9e4991f1d73063f2c14b4beab98ca68cd9c17
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-25 20:59:43 -0300

    Desafio WHILE

---
+++ exercicios/src/controle/DesafioWhile.java
@@ -0,0 +1,32 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int quantidadeDeNotas = 0;
+		double nota = 0;
+		double total = 0;
+		
+		while (nota != -1) {
+			System.out.println("Digire a nota (ou -1 p/ sair): ");
+			nota = entrada.nextDouble();
+			
+			if (nota >= 0 && nota <= 10) {
+				total += nota;
+				quantidadeDeNotas++;
+			} else if (nota != -1) {
+				System.out.println("Numero inválido!!!");
+			}
+		}
+		
+		double media = total / quantidadeDeNotas;
+		System.out.println("Média = " + media);
+		
+		entrada.close();
+	}
+}
