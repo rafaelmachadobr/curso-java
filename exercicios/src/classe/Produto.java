10c10df161aca423a89f1ebffcec98ae92b44247
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-26 16:32:42 -0300

    Construtor

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -6,6 +6,15 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto() {
+	}
+
+	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+	}
+
 	double precoComDesconto() {
 		return preco * (1 - desconto);
 	}
