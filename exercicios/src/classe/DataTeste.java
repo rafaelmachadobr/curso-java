cc69a20fd46f86d022d58a28bd0c68e31f2e4d73
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-26 16:28:32 -0300

    Desafio Método

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -9,13 +9,18 @@ public class DataTeste {
 		d1.mes = 11;
 		d1.ano = 2021;
 		
-		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-		
 		Data d2 = new Data();
 		d2.dia = 31;
 		d2.mes = 12;
 		d2.ano = 2020;
 		
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		String dataFormatada1 = d1.obterDataFormatada();
+		
+		System.out.println(dataFormatada1);
+		System.out.println(d2.obterDataFormatada());
+		
+		d1.imprimirDataFormatada();
+		d2.imprimirDataFormatada();
+		
 	}
 }
