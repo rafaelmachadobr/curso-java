197f42d5afc08779f1751ecc9d51ce4263148cfe
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-30 21:56:29 -0300

    Desafio Herança

--- exercicios/src/oo/heranca/Heroi.java
+++ exercicios/src/oo/heranca/Heroi.java
@@ -2,6 +2,10 @@ package oo.heranca;
 
 public class Heroi extends Jogador {
 	
+	Heroi(int x, int y) {
+		super(x, y);
+	}
+	
 	boolean atacar(Jogador openente) {
 		boolean ataque1 = super.atacar(openente);
 		boolean ataque2 = super.atacar(openente);
