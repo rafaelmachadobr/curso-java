93b2e7c2b0aae926095ed2bb2a3533c05270bcb9
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-31 15:47:03 -0300

    Interfaces Funcionais

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -1,6 +1,15 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
 	double executar(double a, double b);
+	
+	default String legal() {
+		return "Legal";
+	}
+	
+	default String muitoLegal() {
+		return "Muito legal";
+	}
 }
