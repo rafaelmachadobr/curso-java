38f45c9b136f4e083a9923a8d4adcefee256b42a
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-03 13:59:04 -0300

    Desafio Map

--- exercicios/src/streams/Utilitarios.java
+++ exercicios/src/streams/Utilitarios.java
@@ -3,6 +3,9 @@ package streams;
 import java.util.function.UnaryOperator;
 
 public class Utilitarios {
+	
+	private Utilitarios() {
+	}
 
 	public final static UnaryOperator<String> maiuscula = n -> n.toUpperCase();
 	
